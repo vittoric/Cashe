62c02b5c723a3f07ddb4cadf9c441121999c1c32
dolvin17 <huertas876@gmail.com>
2024-01-28 02:16:44 +0100

    index casi ready

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -42,18 +42,28 @@ export default function Navigation() {
 
   </div>
 </div>
-	<section
+<section
+  style={{ position: "absolute" }}
+  className={`flex-col bg-opacity-50 max-w-[20rem] h-64 items-center mt-20 ml-[300px] inset-7 border-l-purple-100 !z-50 py-4 bg-blue-900 [&_button]:underline [&_button]:underline-offset-8 rounded-xl drop-shadow-lg backdrop-blur-lg border-white text-white gap-2  
+    ${isOpen ? "flex" : "hidden"}`}
+>
+	<a href="#" className="block w-full py-2 text-lg font-medium text-center hover:text-blue-500">
+    Mision
+  </a>
+  
+  <a href="../quienes-somos" className="block w-full py-2 text-lg font-medium text-center hover:text-blue-500">
+    Coleccion de peliculas
+  </a>
+  <a href="../quienes-somos" className="block w-full py-2 text-lg font-medium text-center hover:text-blue-500">
+    Coleccion de NFTs
+  </a>
+  <a href="../contacto" className="block w-full py-2 text-lg font-medium text-center hover:text-blue-500">
+    Contacto
+  </a>
+</section>
 
-	style={{position:"absolute",}}
-        className={`flex-col max-w-[20rem] h-2/6 items-center mt-20 ml-[300px] inset-7  !z-50 py-4 bg-white [&_button]:underline [&_button]:underline-offset-8 rounded-xl drop-shadow-lg bg-white/80 backdrop-blur-lg border-white  text-black gap-2  
-		${isOpen ? "flex" : " hidden"}`}	
-      >
-        Quienes somos <br></br>
-		Quienes somos <br></br>
-		Quienes somos <br></br>
-		Quienes somos <br></br>
-		
-      </section>
+
+
 </div>
 
   );
--- components/Stage/Stage.tsx
+++ components/Stage/Stage.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function Stage() {
   return (
-    <div className="grid flex-wrap items-center justify-center grid-cols-2 px-8 pt-24 sm:pt-36">
+    <div className="grid flex-wrap items-center justify-center grid-cols-2 px-8 sm:pt-36">
       <div className="flex flex-col items-center justify-center">
-        <div className="px-24 mt-4 font-bold text-right skew-x-6 -trangrayslate-y-8 text- text-7xl sm:text-5xl shadow-color-black">
+        <div className="px-24 mt-48 font-mono font-bold text-right skew-x-6 -trangrayslate-y-8 text- text-7xl sm:text-5xl shadow-color-black">
           Llevando la pirateria hacia un nuevo Puerto
         </div>
-        <div className="max-w-lg mt-4 ml-12 text-black shadow-color-black sm:text-2xl">
+        <div className="max-w-lg mt-4 ml-12 text-white shadow-color-black sm:text-2xl">
           We are a Marketplace lorem ipsum
           <p className="mt-4 ml-4 text-sm">
             Our dapp ensures seamless transactions,
@@ -26,8 +26,8 @@ export default function Stage() {
       </div>
       <div className="static flex flex-col items-center justify-center">
         <div className="flex flex-row ">
-          <div className="transition duration-700 delay-100 -translate-x-4 -translate-y-3 hover:scale-150">
-            <Image src="/logo.svg" alt="" width={600} height={600} />
+          <div className="animate-jump animate-infinite animate-duration-[7000ms] animate-delay-[800ms] animate-ease-in">
+            <Image src="/graph.svg" alt="" width={700} height={700} />
           </div>
         </div>
       </div>
